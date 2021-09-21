0747bfcfaf3c9d2a835e7a5b42ad5aa33be71c4d
jin <lilijk@hanmail.net>
2021-09-21 14:41:53 +0900

    chore: finish blur uploading

--- p/index.js
+++ p/index.js
@@ -1,6 +1,19 @@
-const icon = document.querySelector('.icon')
-const search = document.querySelector('.search')
+const count = document.querySelector('.count')
+const container = document.querySelector('.container')
 
-icon.addEventListener('click', () => {
-    search.classList.toggle('active')
+let idx = 0
+
+window.addEventListener('load', () => {
+    let increment = setInterval(() => {
+        idx++
+
+        // bg opacity
+        count.textContent = idx + "%" 
+        count.style.opacity = `${1 - (idx / 100)}`
+        container.style.filter = `blur(${25 - (idx / 4)}px)`
+        if (idx >= 4000) {
+            clearInterval(increment)
+            idx = 0
+        }
+     }, 10);
 })
