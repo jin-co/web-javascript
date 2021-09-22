156d13ae875f80d9863037692dd5bc558c65f1b8
jin <lilijk@hanmail.net>
2021-09-23 01:15:52 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -4,13 +4,15 @@ const contents = document.querySelectorAll('.content')
 window.addEventListener('wheel', () => {
     console.log(window.innerHeight)
     contents.forEach(content => {
-        if (window.innerHeight -100 < content.getBoundingClientRect().top) {
+        if ((window.innerHeight / 2) +150 > content.getBoundingClientRect().top) {
             content.classList.add('show')
-            console.log(content.getBoundingClientRect().top)
+            console.log("window: ", window.innerHeight / 2 + 100)
+            console.log("client top: ", content.getBoundingClientRect().top)
             // const box = document.createElement('div')
             // box.classList.add('content')
             // container.appendChild(box)
-            console.log('h')
+        } else {
+            content.classList.remove('show')
         }
     });  
 })
