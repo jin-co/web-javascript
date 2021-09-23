a9d2a6163798fe69fb0644019fe114e5af2a4616
jin <lilijk@hanmail.net>
2021-09-23 12:13:15 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -4,11 +4,13 @@ const containers = document.querySelectorAll('.container')
 containers.forEach(container => {
     container.addEventListener('mouseenter', () => {
         container.style.width = '280%'
+        container.classList.add('active')
     })
 });
 
 containers.forEach(container => {
     container.addEventListener('mouseleave', () => {
         container.style.width = '100%'
+        container.classList.remove('active')
     })
 });
