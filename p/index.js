b7524c1a6558a31c341f1c1e666a5702e19d70b9
jin-co <lilijk@hanmail.net>
2021-09-27 12:00:10 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -1,19 +1,28 @@
 const inputTexts = document.querySelectorAll('.input-text')
 const inputBoxes = document.querySelectorAll('.input-box')
 const container = document.querySelector('.container')
-
+const eachWords = document.querySelectorAll('.each-word')
 
 inputBoxes.forEach(box => {
     box.addEventListener('click', () => {
-        let words = box.previousElementSibling.textContent.split('')
-        console.log(typeof(words))
-        words.forEach((word, idx) => {
-            console.log(word)
+        eachWords.forEach((eachWord, idx) => {
             setTimeout(() => {
-                word.style.color = 'red'
-                word.style.transform = 'translateY(-10px)'
-            }, idx * 30);
+                eachWord.style.transform = 'translateY(0)'
+                eachWord.style.color = 'red'    
+            }, idx * 20);
         });
+
+        // let words = box.previousElementSibling.textContent.split('')
+        // console.log('words type: ', typeof(words))
+        // console.log('words array? ', words)
+        // words[0].style.transform = 'translateY(-20px)'
+        // words.forEach((word, idx) => {
+        //     console.log(word)
+        //     setTimeout(() => {
+        //         word.style.color = 'red'
+        //         word.style.transform = 'translateY(-10px)'
+        //     }, idx * 30);
+        // });
         
         // inputTexts.forEach(text => {
         //     let words = text.textContent.split('')
