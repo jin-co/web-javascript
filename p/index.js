42fe7e9b083d7ec98e9a6f7dc396641a2c10b5c3
jin-co <lilijk@hanmail.net>
2021-09-30 11:21:22 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -1,8 +1,9 @@
-const inputTexts = document.querySelectorAll('.input-text')
-const inputBoxes = document.querySelectorAll('.input-box')
-const container = document.querySelector('.container')
-const eachWords1 = document.querySelectorAll('.each-word1')
-const eachWords2 = document.querySelectorAll('.each-word2')
+const labels = document.querySelectorAll('.form-control label');
 
-
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('')
+});
 
