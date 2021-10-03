3e8b5fe27c22083e465eff9382bc767aadcc1e0c
jin-co <lilijk@hanmail.net>
2021-10-03 11:57:33 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -1,3 +1,6 @@
-const letters = document.querySelector('.input-control label')
+const letters = document.querySelectorAll('.input-control label')
 
-letters
+letters.forEach(letter => {
+    letter.innerHTML = letter.textContent.split('').map((span, idx) => 
+    `<span style="transition-delay:${idx * 50}ms">${span}</span>`).join('')
+});
