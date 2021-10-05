73bb9008c1589fa7efe8292a223389a8c4f105da
jin-co <lilijk@hanmail.net>
2021-10-05 11:17:06 +0900

    chore: update

--- p/index.js
+++
@@ -1,8 +0,0 @@
-const labels = document.querySelectorAll('label')
-
-labels.forEach(label => {
-    label.innerHTML = label.textContent
-    .split('')
-    .map((letter, idx) => `<span style="transition-delay:${idx * 40}ms">${letter}</span>`)
-    .join('')
-});
