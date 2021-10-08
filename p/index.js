3d0f9b95786a474fb0c6eeabdb5c19b3c7d4b889
jin-co <lilijk@hanmail.net>
2021-10-08 11:19:23 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -3,16 +3,36 @@ const API = 'https://v2.jokeapi.dev/joke/Any'
 const btn = document.querySelector('.btn')
 const joke = document.querySelector('.joke')
 
+getJoke()
+
 btn.addEventListener('click', () => {
     getJoke()
 })
 
-async function getJoke() {
-    const res = await fetch(API)
-    const data = await res.json()
-    joke.textContent = data.delivery
-    console.log(data)
-    console.log(data.id)
+// async function getJoke() {
+//     const res = await fetch(API)
+//     const data = await res.json()
+//     joke.textContent = data.delivery
+// }
 
-
+let user = {
+    name: 'j',
+    tell: '123'
 }
+
+function getJoke() {
+    fetch(API, {
+        method: 'POST',
+        headers: {
+            Authentication: 'secret'
+        },
+        body: JSON.stringify(user)
+    }).then(res => res.json()).then(data => {
+        console.log(data, data.id)
+    }).catch(error => {
+
+    })
+}
+
+// https://javascript.info/fetch
+// https://www.w3schools.com/js/js_api_fetch.asp
