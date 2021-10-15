fd3a08a64b8b437f317977552a904388f22953a0
jin-co <lilijk@hanmail.net>
2021-10-15 11:30:10 +0900

    chore: update

---
+++ p/index.js
@@ -0,0 +1,11 @@
+const text = document.querySelector('.text')
+
+text.addEventListener('input', (e) => {
+    let word = ""
+    word += e.target.value
+    console.log(e)
+
+    if (e.target.value == "") {
+        
+    }
+})
