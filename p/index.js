af6d076c04d479c5737551cf7c36fc258ed9796c
jin-co <lilijk@hanmail.net>
2021-10-17 12:28:59 +0900

    chore: udpate

--- p/index.js
+++ p/index.js
@@ -1,11 +1,45 @@
 const text = document.querySelector('.text')
+const btnBox = document.querySelector('.btn-box')
 
-text.addEventListener('input', (e) => {
+let btns = []
+
+text.addEventListener('keydown', (e) => {
     let word = ""
-    word += e.target.value
-    console.log(e)
+    console.log(e.target.value)
 
-    if (e.target.value == "") {
-        
+    if (e.key != ',') {
+        word += e.target.value    
+        console.log(word)
+    }
+    
+    console.log(word)
+    if (e.key == ",") {
+        addChoice(word)
+        e.target.value = ''
+    }
+
+    if (e.key == "Enter") {
+        chooseOne()
+        text.innerHTML = ''
     }
 })
+
+function addChoice(word) {
+    const choiceEl = document.createElement('button')
+    choiceEl.className = 'btn'
+    choiceEl.textContent = word
+    btns.push(choiceEl)
+    btnBox.appendChild(choiceEl)
+}
+
+function chooseOne() {
+    console.log(btns)
+    
+    
+    setTimeout(() => {
+        let num = Math.floor(Math.random() * btns.length)
+        console.log(num)
+        btns[num].className = 'btn chosen'    
+    }, 3000);
+    
+}
