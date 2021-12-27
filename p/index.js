68a27b8902045beb9b7eca13c6b7bb20aac094ed
jin-co <lilijk@hanmail.net>
2021-12-27 12:00:57 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -1,65 +1,7 @@
-const text = document.querySelector('.text')
-const btnBox = document.querySelector('.btn-box')
+const btn = document.querySelector('.btn')
+const nav = document.querySelector('.nav')
 
-let btns = []
-let word = ""
-let words = []
-text.focus()
-flag = false
-let btnEl = null
 
-text.addEventListener('keydown', (e) => {
-    const lastBtnEl = document.querySelectorAll('.btn')
-    
-    if (e.key == 'Enter') {
-        chooseOne(lastBtnEl)
-        return
-    }
-
-    if (!flag && e.key != 'Backspace') {
-        btnEl = document.createElement('button')
-        btnEl.className = 'btn'
-        btnBox.appendChild(btnEl)
-        flag = true
-    }    
-
-    if (e.key == ',') {
-        flag = false
-    }
-
-    if (e.key == 'Backspace') {                
-        if (lastBtnEl[lastBtnEl.length - 1].textContent.length <= 0) {
-            lastBtnEl[lastBtnEl.length - 1].remove()
-            flag = false
-        }      
-
-        if (lastBtnEl.length > 0) {
-            let words = lastBtnEl[lastBtnEl.length - 1].textContent.split('')        
-            let aa = words.splice(-1, 1)
-            lastBtnEl[lastBtnEl.length - 1].textContent = words.join('')
-            console.log('in: ', flag)
-        }        
-    }
-
-    if (e.key != ',' && e.key != 'Backspace') {
-        btnEl.textContent += e.key
-    }    
+btn.addEventListener('click', () => {
+    nav.classList.toggle('show')
 })
-
-function chooseOne(lastBtnEl) {   
-    let random
-    const effect = setInterval(() => {
-        lastBtnEl.forEach(btn => {
-            btn.classList.remove('chosen')
-        });
-        random = Math.floor(Math.random() * lastBtnEl.length)
-        lastBtnEl[random].classList.add('chosen')
-    }, 100);
-    
-    setTimeout(() => {
-        lastBtnEl[random].classList.add('chosen')
-        clearInterval(effect)
-    }, 3000)        
-}
-
-
