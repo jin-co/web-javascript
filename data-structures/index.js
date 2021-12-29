16d7f9cea758bd3b0b5169a3380770111ce25abc
jin-co <lilijk@hanmail.net>
2021-12-29 14:45:16 +0900

    chore: update

--- data-structures/index.js
+++ data-structures/index.js
@@ -1,9 +1,18 @@
+// function addUpTo(n) {
+//     let total = 0;
+//     for (var i = 1; i <= n; i++) {
+//         total += i;
+//     }
+//     return total;
+// }
+
 function addUpTo(n) {
-    let total = 0;
-    for (var i = 1; i <= n; i++) {
-        total += i;
-    }
-    return total;
+    return n * (n + 1) / 2;
 }
 
-console.log(addUpTo(6))
+
+// // performance tester
+// let t1 = performance.now(); // total time since opening the browser
+// console.log(addUpTo(6))
+// let t2 = performance.now();
+// console.log(`total time: ${(t2 - t1) / 1000}s`)
