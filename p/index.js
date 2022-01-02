ed84de98d309a6ca9c14e02d4c9841e901ca0c61
jin-co <lilijk@hanmail.net>
2022-01-02 11:42:49 +0900

    chore: update

--- p/index.js
+++ p/index.js
@@ -1,7 +1,24 @@
-const btn = document.querySelector('.btn')
-const navBox = document.querySelector('.nav-box')
+const numTwitter = document.querySelector('.num-twitter')
+const numYoutube = document.querySelector('.num-youtube')
+const numFacebook = document.querySelector('.num-facebook')
 
+const TWITTER_MAX = 12000
+const YOUTUBE_MAX = 5000
+const FACEBOOK_MAX = 7500
 
-btn.addEventListener('click', () => {
-    navBox.classList.toggle('show')
-})
+const test = document.querySelector('.test')
+
+setTimeout(() => {
+    numTwitter.textContent = numTwitter.textContent ++
+    console.log('h')
+}, 100)
+
+let increment = setInterval(() => {    
+    if (test.value < +test.max - 1) {
+        test.value = +test.value + 10
+        console.log(test.value++, +test.max)
+    } else {
+        clearInterval(increment)
+    }
+    console.log(test.value++, test.max)    
+}, 1)
