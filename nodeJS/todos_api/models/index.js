6d52dec145574791db6c541e8108fdc526148b49
jin-co <lilijk@hanmail.net>
2022-01-28 19:16:54 +0900

    chore: update

--- nodeJS/todos_api/models/index.js
+++ nodeJS/todos_api/models/index.js
@@ -2,3 +2,5 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true)
 mongoose.connect('mongodb//localhost/todo-api')
 mongoose.Promise = Promise  // to use promise syntax instead of callback
+
+module.exports.Todo = require('./todo')
--- nodeJS/todos_api/routes/todos.js
+++ nodeJS/todos_api/routes/todos.js
@@ -2,8 +2,17 @@
 
 var express = require('express')
 var router = express.Router()
+var db = require('../models')
+
+
 router.get('/', function(req, res) {
-    res.send("hello route")
+    db.Todo.find()
+    .then(function(todos) {
+        res.json(todos)
+    })
+    .catch(function(err) {
+        res.send(err)
+    })
 })
 
 module.exports = router
