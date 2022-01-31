433e1f133a5255efce784d013a908d7483421f3a
jin-co <lilijk@hanmail.net>
2022-01-31 15:00:01 +0900

    chore: update

--- nodeJS/todos_api/index.js
+++ nodeJS/todos_api/index.js
@@ -1,9 +1,13 @@
 var express = require('express'), 
     app = express(), 
-    port = process.env.PORT || 8080
+    port = process.env.PORT || 8080,
+    bodyParser = require('body-parser')
 
 // importing route
 var todoRoutes = require('./routes/todos')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 app.get('/', function(req, res) {
     res.send('hi there')
--- nodeJS/todos_api/routes/todos.js
+++ nodeJS/todos_api/routes/todos.js
@@ -15,4 +15,45 @@ router.get('/', function(req, res) {
     })
 })
 
+router.post('/', function(req, res) {
+    db.Todo.create(req.body)
+    .then(function(newTodo) {
+
+    })
+    .catch(function(err) {
+        res.send(err)
+    })
+})
+
+router.get('/:todoId', function(req, res) {
+    db.Todo.findById(req.params.todoId)
+    .then(function(found) {
+        res.json(found)
+    })
+    .catch(function(err) {
+        res.send(err)
+    })
+})
+
+router.put('/:todoId', function(req, res) {
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+    .then(function(todo) {
+        res.json(todo)
+    })
+    .catch(function(err) {
+        res.send(err)
+    })
+})
+
+// delete
+router.delete('/:todoId', function(req, res) {
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function() {
+        res.json({message: 'deleted'})
+    })
+    .catch(function(err) {
+        res.send(err)
+    })
+})
+
 module.exports = router
