d740c8f051a667c41acda9e1db3d758230dd9914
jin-co <lilijk@hanmail.net>
2022-02-21 17:38:36 +0900

    Update

--- nodeJS/with_angular/agular_node1/backend/app.js
+++ nodeJS/with_angular/agular_node1/backend/app.js
@@ -43,6 +43,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Post = require('./models/post')
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -62,7 +64,11 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  // const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
