b95d1e383f03918ef518605088ab7f8731c76612
jin-co <lilijk@hanmail.net>
2022-02-23 14:03:37 +0900

    Update

--- nodeJS/with_angular/e_commerce/app.js
+++ nodeJS/with_angular/e_commerce/app.js
@@ -10,26 +10,51 @@ const api = process.env.API_URL;
 app.use(express.json())
 app.use(morgan('tiny'));
 
-app.get(`/`, (req, res) => {
-    product = {
-        id: 1,
-        name: 'tom'
-    }
-    res.send(product)
+// app.get(`/`, (req, res) => {
+//     product = {
+//         id: 1,
+//         name: 'tom'
+//     }
+//     res.send(product)
+// })
+
+// schema
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
 })
 
+// model
+const Product = mongoose.model('Product', productSchema);
+
+
+
 app.post(`/`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct)
+    // const newProduct = req.body;
+    // console.log(newProduct);
+    // res.send(newProduct)
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err) => {
+        res.status(500).json({ // for this i can create custom object
+            error: err,
+            success: false
+        })
+    })    
 })
 
 app.get(`${api}products`, (req, res) => {
-    product = {
-        id: 1,
-        name: 'tom'
-    }
-    res.send(product)
+    // product = {
+    //     id: 1,
+    //     name: 'tom'
+    // }
+    // res.send(product)
 })
 
 mongoose.connect(process.env.CONNECTION)
