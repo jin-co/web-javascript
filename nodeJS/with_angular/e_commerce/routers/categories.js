5c5f06c4665d22932ca71406621f2786b2ee0536
jin-co <lilijk@hanmail.net>
2022-03-01 13:58:30 +0900

    Update

--- nodeJS/with_angular/e_commerce/routers/categories.js
+++ nodeJS/with_angular/e_commerce/routers/categories.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Category = require('../models/category');
 
-router.get('/', (req, res) => {
-    
+router.get('/', async (req, res) => {
+    const categoryList = await Category.find();
+
+    if(!categoryList) {
+        res.status(500).json({success: false})
+    }
+    res.status(200).send(categoryList);
+})
+
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if(!category) {
+        return res.status(500).json({message: "Category search error"})
+    }
+    res.status(200).send(category);
 })
 
 router.post('/', async (req, res) => {
@@ -39,6 +52,21 @@ router.delete('/:id', (req, res) => {
             error: err
         })
     })
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findByIDAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        }
+    )
+    if(!category) {
+        return res.status(400).json({message: "Category update error"})
+    }
+    res.send(category);
 })
 
 module.exports = router;
