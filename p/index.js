2bfde48b0ede659f5e5b3f9445628513b55fac45
jin-co <lilijk@hanmail.net>
2022-03-09 13:04:40 -0500

    Update

--- p/index.js
+++ p/index.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', (e) => {
-    console.log(e.target)
-    console.log(btn.getBoundingClientRect())
+    console.log('e x: ', e.clientX, 'e y: ', e.clientY)    
+    console.log('rec x: ', btn.getBoundingClientRect().x, 'rect y: ', btn.getBoundingClientRect().y)
+    console.log('sub x', e.clientX - btn.getBoundingClientRect().x)
+    console.log('sub y', e.clientY - btn.getBoundingClientRect().y)
 })
