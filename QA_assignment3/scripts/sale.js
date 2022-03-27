40b20454b223e98bbca5a0ed9552caf78e8b6b3b
jin-co <lilijk@hanmail.net>
2022-03-27 15:31:39 -0400

    Added getting form value

--- QA_assignment3/scripts/sale.js
+++ QA_assignment3/scripts/sale.js
@@ -1,14 +1,26 @@
 const btn = document.querySelector(".btn");
-const formData = document.querySelector(".form");
+let formData = document.querySelectorAll(".form input");
+// let form = document.querySelector('.form');
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  // test
-  let data = new FormData(formData);
 
-//   data.append("firstName", document.getElementById("first-name").value);
-//   data.append("lastName", document.getElementById("last-name").value);
+  console.log(formData);
+  let newArray = Array.from(formData).reduce(
+    (acc, input) => ({ ...acc, [input.id]: input.value }),
+    {}
+  );
 
-  console.log(data);
-  
+  // let formD = new FormData(form);
+
+  // this is only to add additional data
+  // formD.append('api-key', 'my-key');
+
+  console.log(newArray);
+  // console.log(formD);
+  // for(let key of formD.keys()) {
+  //   console.log(key, formD.get(key))
+  // }
+
+  // window.location.href = '/pages/list.html'
 });
