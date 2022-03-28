ea52bf1406f1f4185ef2c402e5a15ece64e990e7
jin-co <lilijk@hanmail.net>
2022-03-28 12:26:53 -0400

    Completed summary page

--- QA_assignment3/scripts/summary.js
+++ QA_assignment3/scripts/summary.js
@@ -1,12 +1,38 @@
+const summaryName = document.querySelector
+const summaries = document.querySelectorAll('.summary')
+const confirmBtn = document.querySelector('.btn-summary-confirm')
+
 let tempData = {};
+let dataList = []
 
 tempData = JSON.parse(localStorage.getItem("tempData"));
 
+createSummary();
 
+console.log(tempData)
 
-console.log(tempData);
-
-
-function createList() {
-    
+function createSummary() {
+    summaries[0].innerHTML = tempData["first-name"] + " " + tempData["last-name"]
+    summaries[1].innerHTML = tempData.address     
+    summaries[2].innerHTML = tempData.city 
+    summaries[3].innerHTML = tempData.province 
+    summaries[4].innerHTML = tempData.postal
+    summaries[5].innerHTML = tempData.phone 
+    summaries[6].innerHTML = tempData.email 
+    summaries[7].innerHTML = tempData.make
+    summaries[8].innerHTML = tempData.model
+    summaries[9].innerHTML = tempData.year
 }
+
+confirmBtn.addEventListener('click', () => {
+    let storedData = localStorage.getItem("dataList")
+    if (storedData !== null) {
+        dataList = JSON.parse(storedData)
+    }
+
+    dataList.push(tempData)
+
+    localStorage.setItem("dataList", JSON.stringify(dataList))
+
+    window.location.href = '/pages/list.html'
+})
