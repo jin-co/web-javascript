4b8788243572efc0c067c2f9f515d9397649d573
jin-co <lilijk@hanmail.net>
2022-04-21 22:42:01 -0400

    Update

---
+++ nodeJS/with_angular/p1_postApp/src/app/components/posts/post-list/post-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts = [
+    {title: 'First', content: "1"},
+    {title: 'second', content: "1"},
+    {title: 'third', content: "1"},
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
