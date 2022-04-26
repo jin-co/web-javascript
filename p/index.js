05b5b0ff5148a575cb1d602bb57a81ecbcaa01c5
jin-co <lilijk@hanmail.net>
2022-04-25 20:55:23 -0400

    Update

--- p/index.js
+++ p/index.js
@@ -2,22 +2,21 @@ const img = document.querySelector("img");
 const boxes = document.querySelectorAll(".box");
 
 img.addEventListener("dragstart", (e) => {
-    
   e.dataTransfer.setData("image", e.target.id);
-  console.log(e)
+  console.log(e);
 });
 
 boxes.forEach((b) => {
   b.addEventListener("dragover", (e) => {
-    e.preventDefault()
+    e.preventDefault();
   });
 });
 
 boxes.forEach((b) => {
   b.addEventListener("drop", (e) => {
-      console.log('drop')
-    e.preventDefault()
-    var data = e.dataTransfer.getData("image")
-    e.target.appendChild(document.getElementById(data))
+    console.log("drop");
+    e.preventDefault();
+    var data = e.dataTransfer.getData("image");
+    e.target.appendChild(document.getElementById(data));
   });
 });
