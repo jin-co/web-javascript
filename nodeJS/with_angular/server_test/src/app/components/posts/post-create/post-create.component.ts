d12cb5ff4269a99590ce58f1ed0c2bc24cb39c4c
jin-co <lilijk@hanmail.net>
2022-04-27 09:47:18 -0400

    Added validation

--- nodeJS/with_angular/server_test/src/app/components/posts/post-create/post-create.component.ts
+++ nodeJS/with_angular/server_test/src/app/components/posts/post-create/post-create.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from 'src/models/post';
 import { PostService } from 'src/services/post.service';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
-  inputTitle:String = ''
-  inputContent:String = ''
-  outputTitle:String = ''
-  outputContent:String = ''
+  inputTitle: String = '';
+  inputContent: String = '';
+  outputTitle: String = '';
+  outputContent: String = '';
 
-  postCreated = new EventEmitter<Post>()
+  postCreated = new EventEmitter<Post>();
 
-  constructor(public postService:PostService) { }
+  constructor(public postService: PostService) {}
 
-  ngOnInit(): void {
-  }
-  
-  onClick() {    
-    this.postService.setPost(this.inputTitle, this.inputContent)
+  ngOnInit(): void {}
+
+  onClick(form: NgForm) {
+    if (form.valid) {
+      this.postService.setPost(form.value.title, form.value.content);
+    } else {
+      return;
+    }
   }
 }
