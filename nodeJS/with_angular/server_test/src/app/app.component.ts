2f4803bc7a83e3f0aa62ed4836544879931e07d7
jin-co <lilijk@hanmail.net>
2022-04-26 21:14:37 -0400

    Completed event emitter

--- nodeJS/with_angular/server_test/src/app/app.component.ts
+++ nodeJS/with_angular/server_test/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'server_test';
+  posts:any = []
+
+  addPost(e:any) {
+    this.posts.push(e)
+  }
 }
