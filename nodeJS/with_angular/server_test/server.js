5371aec65c65370918884c3fbc68fa26ad78e1ce
jin-co <lilijk@hanmail.net>
2022-04-28 19:24:36 -0400

    Added components

--- nodeJS/with_angular/server_test/server.js
+++ nodeJS/with_angular/server_test/server.js
@@ -2,6 +2,8 @@ const http = require('http')
 
 const app = require('./backend/app')
 
+const port = process.env.PORT || 3000
+
 const server = http.createServer(app)
 
-server.listen(process.env.PORT || 3000)
+server.listen(port)
--- nodeJS/with_angular/server_test/src/app/app.module.ts
+++ nodeJS/with_angular/server_test/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
