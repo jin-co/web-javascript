36aa6c76bb06db03d135d33a6d977cec56f65dc2
jin-co <lilijk@hanmail.net>
2022-04-28 11:31:05 -0400

    Update

--- nodeJS/with_angular/p1_postApp/backend/app.js
+++ nodeJS/with_angular/p1_postApp/backend/app.js
@@ -1,12 +1,26 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-app.use((req, res, next) => {
-    next()
-})
+const app = express();
+// app.use((req, res, next) => {
+//     next()
+// })
 
-app.use((req, res, next) => {
-    res.send("heih")
-})
+// app.use((req, res, next) => {
+//     res.send("heih")
+// })
 
-module.exports = app
+app.use("/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: 1,
+      title: "hi",
+      content: "me",
+    },
+  ];
+  res.status(200).json({
+      message: 'post',
+      posts: posts
+  });
+});
+
+module.exports = app;
