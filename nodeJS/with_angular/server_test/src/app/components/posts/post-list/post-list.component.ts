a5a7bd4adbd7bde19869ae921288e4943fab9591
jin-co <lilijk@hanmail.net>
2022-04-29 22:45:47 -0400

    Update

--- nodeJS/with_angular/server_test/src/app/components/posts/post-list/post-list.component.ts
+++ nodeJS/with_angular/server_test/src/app/components/posts/post-list/post-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/post.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent implements OnInit {
+  posts: any = [];
 
-  constructor() { }
+  constructor(public postService: PostService) {}
 
   ngOnInit(): void {
+    this.posts = this.postService.getPosts();
+    this.postService.updateListener().subscribe((posts: any) => {
+      this.posts = posts;
+    });
   }
-
 }
--- nodeJS/with_angular/server_test/src/post.service.ts
+++ nodeJS/with_angular/server_test/src/post.service.ts
@@ -1,13 +1,22 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
+@Injectable({ providedIn: 'root' })
 export class PostService {
+    posts = []
   constructor(private http: HttpClient) {}
-    baseURL:String = 'http://localhost:3000/'
-  setPosts() {
-    
-  }
+  baseURL: String = 'http://localhost:3000/';    
+  postUpdated = new Subject();
+  setPosts() {}
 
   getPosts() {
-    this.http.get(`${this.baseURL}posts`)
+    this.http.get<{message:string, posts:any}>(`${this.baseURL}posts`).subscribe((data) => {
+        this.posts = data.posts
+    })
+  }
+
+  updateListener() {
+      return this.postUpdated.asObservable()
   }
 }
