8bd5d8bf607cc708d51db52e9dd778724070354b
jin-co <lilijk@hanmail.net>
2022-04-30 12:33:50 -0400

    Update

--- p/index.js
+++ p/index.js
@@ -7,46 +7,41 @@ const clearEl = document.querySelector('#clear')
 
 const ctx = canvas.getContext("2d")
 
-let size = 5
+let size = +sizeEl.innerHTML
 let clicked = false
-colorEl.value = 'black'
-let color = colorEl.value
 let x
 let y
-
 canvas.addEventListener('mousedown', (e) => {
   clicked = true
+
   x = e.offsetX
   y = e.offsetY
 
-  console.log('x:', x, 'y:', y)
+  console.log('down')
 })
 
 canvas.addEventListener('mouseup', (e) => {
-  clicked = false  
+  clicked = false
+  console.log('up')
 })
 
 canvas.addEventListener('mousemove', (e) => {
   if(clicked) {
+    console.log('move')
     const x2 = e.offsetX
     const y2 = e.offsetY
-    drawCircle(x2, y2)
-    // drawLine(x, y, x2, y2)
+
+    drawLine(x, y, x2, y2)
+
     x = x2
     y = y2
   }
 })
 
-function drawLine(x1, y1, x2, y2) {  
+function drawLine(x1, y1, x2, y2) {
   ctx.beginPath()
-  ctx.moveTo(x1, x2)
+  ctx.moveTo(x1, y1)
   ctx.lineTo(x2, y2)
-  ctx.lineWidth = size * 2 // making line with the 
+  ctx.lineWidth = size * 2
   ctx.stroke()
-}
-
-function drawCircle(x, y) {
-  ctx.beginPath()
-  ctx.arc(x, y, size, 0, Math.PI * 2)
-  ctx.fill()
 }
