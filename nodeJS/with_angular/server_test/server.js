c7fd6402340b807028d3412ba6dabf1431af371b
jin-co <lilijk@hanmail.net>
2022-05-01 11:19:53 -0400

    Moved server

--- nodeJS/with_angular/server_test/server.js
+++ nodeJS/with_angular/server_test/server.js
@@ -1,7 +1,5 @@
 const http = require('http')
 const port = process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-    res.end('server up')
-})
-
+const app = require('./backend/app')
+const server = http.createServer(app)
 server.listen(port)
