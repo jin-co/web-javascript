a609ba41491ff6be0743f78b90f9bebd537d50a3
jin-co <lilijk@hanmail.net>
2022-05-01 11:27:43 -0400

    Update

--- nodeJS/with_angular/server_test/backend/app.js
+++ nodeJS/with_angular/server_test/backend/app.js
@@ -3,7 +3,17 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) => {
-    res.send('app server')
+    
+
+    next()
+})
+
+app.get('/posts',(req, res, next) => {
+    const post = [
+        {title: 'tom', content: 'jack'}
+    ]
+
+    res.json(post)
 })
 
 
