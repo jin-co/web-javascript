86bff449071342fdc5ef7b469ee7453653a9cd0f
jin-co <lilijk@hanmail.net>
2022-05-03 19:44:32 -0400

    Connected to db

--- nodeJS/with_angular/post_test/backend/app.js
+++ nodeJS/with_angular/post_test/backend/app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String,
+});
+const post = mongoose.model("Post", postSchema);
+const dbURL =
+  "mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/posts?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURL)
+  .then(() => console.log("connected to db"))
+  .catch(() => console.log("connection failed"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
