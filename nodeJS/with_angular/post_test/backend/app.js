eb31a88dace5260e2969c8e2c653a346ae97d59e
jin-co <lilijk@hanmail.net>
2022-05-03 12:47:52 -0400

    init post test

---
+++ nodeJS/with_angular/post_test/backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, FETCH, POST, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+});
+
+app.use((req, res, next) => {
+    res.send('hsi')
+})
+
+module.exports = app;
---
+++ nodeJS/with_angular/post_test/server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+const app = require('./backend/app')
+
+const server = http.createServer(app)
+
+server.listen(port)
+
