50002ec80803d9c5c7f1b504c3a5a3d5c25b83cf
jin-co <lilijk@hanmail.net>
2022-05-03 20:46:43 -0400

    Update

--- nodeJS/with_angular/post_test/backend/app.js
+++ nodeJS/with_angular/post_test/backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 const postSchema = mongoose.Schema({
   title: String,
   content: String,
 });
-const post = mongoose.model("Post", postSchema);
+
 const dbURL =
   "mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/posts?retryWrites=true&w=majority";
 mongoose
@@ -13,22 +15,39 @@ mongoose
   .then(() => console.log("connected to db"))
   .catch(() => console.log("connection failed"));
 
+const Post = mongoose.model("Post", postSchema);
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, FETCH, POST, DELETE, OPTIONS"
-  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("hsi");
+app.get("/posts", (req, res, next) => {
+  Post.find((err, result) => {
+    if (!err) {
+      console.log(result);
+      res.json(result);
+    }
+  });
+});
+
+app.post("/posts", jsonParser, (req, res, next) => {
+    console.log(req.body)
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((result) => {
+    
+  });
 });
 
 module.exports = app;
--- nodeJS/with_angular/post_test/src/services/post.service.ts
+++ nodeJS/with_angular/post_test/src/services/post.service.ts
@@ -1,27 +1,37 @@
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
-import { Post } from "src/models/post";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Post } from 'src/models/post';
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
 export class PostService {
-    posts:Post[] = []
-    updatedPost = new Subject<Post[]>()
+  posts: Post[] = [];
+  updatedPost = new Subject<Post[]>();
+  baseURL = 'http://localhost:3000/';
+  constructor(private http: HttpClient) {}
 
-    getPosts() {
-        return this.posts
-    }
+  getPosts() {
+    this.http.get<Post[]>(`${this.baseURL}posts`).subscribe((data) => {
+      this.posts = data;
+      this.updatedPost.next([...this.posts]);
+    });
+    return this.posts;
+  }
 
-    setPost(title:string, content:string) {
-        const post:Post = {
-            _id: '',
-            title: title,
-            content: content
-        }
-        this.posts.push(post)
+  setPost(title: string, content: string) {
+    const post: Post = {
+      _id: '',
+      title: title,
+      content: content,
+    };    
+
+    this.http.post(`${this.baseURL}/posts`, post).subscribe((data) => {
         this.updatedPost.next([...this.posts])
-    }
+    })
 
-    updateListener() {
-        return this.updatedPost.asObservable()
-    }
+  }
+
+  updateListener() {
+    return this.updatedPost.asObservable();
+  }
 }
