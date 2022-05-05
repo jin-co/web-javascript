0450e37689a04c3f245508fd378985701fbb89ae
jin-co <lilijk@hanmail.net>
2022-05-05 13:29:36 -0400

    Refactor

--- nodeJS/with_angular/post_test/backend/app.js
+++ nodeJS/with_angular/post_test/backend/app.js
@@ -2,16 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
-const mongoose = require('mongoose')
-const postSchema = mongoose.Schema({
-    _id:String,
-    title:String,
-    content:String
-})
-const dbURL = 'mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/posts?retryWrites=true&w=majority'
-const Post = mongoose.model("Post", postSchema)
-mongoose.connect(dbURL).then(() => console.log('connected')).catch(() => console.log('failed'))
+const Post = require("./models/Post");
+const mongoose = require("mongoose");
+const dbURL =
+  "mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/posts?retryWrites=true&w=majority";
 
+mongoose
+  .connect(dbURL)
+  .then(() => console.log("connected"))
+  .catch(() => console.log("failed"));
 
 const app = express();
 
@@ -25,15 +24,13 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "POST, GET, PUT, FETCH, OPTIONS, DELETE"
   );
-  next()
+  next();
 });
 
 app.use((req, res, next) => {
-    res.send('up')
-})
+  res.send("up");
+});
 
-app.get("/posts", (req, res, next) => {
+app.get("/posts", (req, res, next) => {});
 
-})
-
-module.exports = app
+module.exports = app;
