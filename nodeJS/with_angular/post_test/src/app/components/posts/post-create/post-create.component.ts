3292427120a016f5b8f3b9b0e857e3695ee0dbe6
jin-co <lilijk@hanmail.net>
2022-05-05 12:42:31 -0400

    Added text interpolation

--- nodeJS/with_angular/post_test/src/app/components/posts/post-create/post-create.component.ts
+++ nodeJS/with_angular/post_test/src/app/components/posts/post-create/post-create.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
+  inputTitle = '';
+  inputContent = '';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick(e:HTMLInputElement) {
+    this.inputTitle = e.value
+    this.inputContent = "ha"
   }
-
 }
