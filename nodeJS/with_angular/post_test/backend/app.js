bafa57c7dc50a1f7b49d2a0d999baf17c6969c90
jin-co <lilijk@hanmail.net>
2022-05-05 13:25:48 -0400

    Connected to db

--- nodeJS/with_angular/post_test/backend/app.js
+++ nodeJS/with_angular/post_test/backend/app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
+const mongoose = require('mongoose')
+const postSchema = mongoose.Schema({
+    _id:String,
+    title:String,
+    content:String
+})
+const dbURL = 'mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/posts?retryWrites=true&w=majority'
+const Post = mongoose.model("Post", postSchema)
+mongoose.connect(dbURL).then(() => console.log('connected')).catch(() => console.log('failed'))
+
+
 const app = express();
 
 app.use((req, res, next) => {
