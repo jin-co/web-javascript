d45c46ddd8289e5dcbcd373169e751640d6a65eb
jin-co <lilijk@hanmail.net>
2022-05-05 21:16:43 -0400

    Refactor

--- nodeJS/with_angular/post_test/backend/app.js
+++ nodeJS/with_angular/post_test/backend/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// const jsonParser = bodyParser.json();
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const postRoute = require("./routes/posts");
 
-const Post = require("./models/Post");
 const mongoose = require("mongoose");
 const dbURL =
   "mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/post-app?retryWrites=true&w=majority";
@@ -28,42 +28,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/posts", (req, res, next) => {
-  Post.find().then((result) => {
-    res.json(result);
-  });
-});
-
-app.post("/posts", jsonParser, (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((result) => {
-    res.status(201).json(result);
-  });
-});
-
-app.delete("/posts/:id", (req, res, next) => {  
-  Post.deleteOne({
-    _id: req.params.id,
-  }).then((result) => {
-    res.status(200).json("Post Deleted");
-  });
-});
-
-app.put("/posts/:id", jsonParser, (req, res, next) => {
-    console.log('back put', req.body)
-    const post = new Post({
-        _id: req.body._id,
-        title: req.body.title,
-        content: req.body.content
-    })
-    Post.updateOne({
-        _id: req.params.id
-    }, post).then(result => {
-        res.status(200).json("update")
-    })
-})
+app.use('/posts', postRoute)
 
 module.exports = app;
