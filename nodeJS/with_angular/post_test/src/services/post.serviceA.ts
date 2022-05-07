1fc77590c28e777adb2a2d443b95a7f1fdb6bdda
jin-co <lilijk@hanmail.net>
2022-05-07 12:42:00 -0400

    Added service

--- nodeJS/with_angular/post_test/src/services/post.serviceA.ts
+++ nodeJS/with_angular/post_test/src/services/post.serviceA.ts
@@ -1,6 +1,20 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Post } from "src/models/post";
 
 @Injectable({providedIn:'root'})
 export class PostService {
+    
+    posts:Post[] = []
+    postUpdated = new Subject()
 
+    getPosts() {}
+    getPost(id:string) {}
+    setPosts(title:string, content:string) {}
+    updatePost(id:string, title:string, content:string) {}
+    deletePost(id:string) {}
+
+    updateListener() {
+        return this.postUpdated.asObservable()
+    }
 }
