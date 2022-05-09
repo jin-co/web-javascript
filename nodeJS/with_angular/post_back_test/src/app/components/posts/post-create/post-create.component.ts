59675ff8f94198a167b827c9fddb432e8118cf1b
jin-co <lilijk@hanmail.net>
2022-05-09 11:45:33 -0400

    Completed event emitter

--- nodeJS/with_angular/post_back_test/src/app/components/posts/post-create/post-create.component.ts
+++ nodeJS/with_angular/post_back_test/src/app/components/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -11,14 +11,20 @@ export class PostCreateComponent implements OnInit {
   inputContent: string = '';
   outputTitle: string = '';
   outputContent: string = '';
+
+  @Output() postCreated = new EventEmitter
+  
   constructor() {}
 
   ngOnInit(): void {}
 
   onClick(form: NgForm) {
     if(form.valid) {
-      this.outputTitle = form.value.title
-      this.outputContent = form.value.content
+      const post:any = {
+        title: form.value.title,
+        content: form.value.content        
+      }      
+      this.postCreated.emit(post)
     }    
   }
 }
--- nodeJS/with_angular/post_back_test/src/app/components/posts/post-list/post-list.component.ts
+++ nodeJS/with_angular/post_back_test/src/app/components/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from 'src/models/post';
 
 @Component({
   selector: 'app-post-list',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-
+  @Input() posts:Post[] = []
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
