fd3a343cf5cff35fe4498ee760bc13e885128601
jin-co <lilijk@hanmail.net>
2022-05-10 11:42:16 -0400

    Completed reactive form

--- nodeJS/with_angular/post_back_test/src/app/components/posts/post-create/post-create.component.ts
+++ nodeJS/with_angular/post_back_test/src/app/components/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from 'src/models/post';
 import { PostService } from 'src/services/post.service';
@@ -10,43 +10,82 @@ import { PostService } from 'src/services/post.service';
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
-  mode:string = 'create'
-  id:string = ''
-  post!:Post 
+  mode: string = 'create';
+  id: string = '';
+  post!: Post;
 
-  constructor(public postService:PostService, private activeRoute:ActivatedRoute) {}
+  constructor(
+    public postService: PostService,
+    private activeRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.activeRoute.paramMap.subscribe((pm:ParamMap) => {
-      if(pm.has('id')) {
-        this.mode = 'edit'
-        let paramId = pm.get('id')
-        if(paramId !== null) {
-          this.id = paramId
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
+
+    this.activeRoute.paramMap.subscribe((pm: ParamMap) => {
+      if (pm.has('id')) {
+        this.mode = 'edit';
+        let paramId = pm.get('id');
+        if (paramId !== null) {
+          this.id = paramId;
         }
 
-        this.postService.getPost(this.id).subscribe(data => {
+        this.postService.getPost(this.id).subscribe((data) => {
           this.post = {
             _id: data._id,
             title: data.title,
-            content: data.content
-          }          
+            content: data.content,
+          };
+        });
+
+        this.form.setValue({
+          title: this.post.title,
+          content: this.post.content
         })
       } else {
-        this.mode = 'create'
-      }      
-    })
+        this.mode = 'create';
+      }
+    });
   }
 
-  onClick(form: NgForm) {
-    if(form.valid) {
-      if(this.mode === 'create') {
-        this.postService.setPost(form.value.title, form.value.content)
+  // onClick(form: NgForm) {
+  //   if(form.valid) {
+  //     if(this.mode === 'create') {
+  //       this.postService.setPost(form.value.title, form.value.content)
+  //     } else {
+  //       console.log('front update', this.id)
+  //       this.postService.updatePost(this.id, form.value.title, form.value.content)
+  //     }
+  //     form.resetForm()
+  //   }
+  // }
+
+  form!: FormGroup;
+  onClick() {
+    if (this.form.valid) {
+      if (this.mode === 'create') {
+        this.postService.setPost(
+          this.form.value.title,
+          this.form.value.content
+        );
       } else {
-        console.log('front update', this.id)
-        this.postService.updatePost(this.id, form.value.title, form.value.content)
+        console.log('front update', this.id);
+        this.postService.updatePost(
+          this.id,
+          this.form.value.title,
+          this.form.value.content
+        );
       }
-      form.resetForm()
-    }    
+      this.form.reset();
+    }
   }
+
+  onFileAdd(e: Event) {}
 }
