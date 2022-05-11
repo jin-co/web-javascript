a913092690f254c7a2fb0e28540bfef88b22fa83
jin-co <lilijk@hanmail.net>
2022-05-11 10:19:34 -0400

    Update

---
+++ p/index.js
@@ -0,0 +1,10 @@
+const btnUp = document.querySelector('.cursor-up')
+const btnDown = document.querySelector('.cursor-down')
+
+btnUp.addEventListener('click', () => {
+    console.log('up')
+})
+
+btnDown.addEventListener('click', () => {
+    console.log('down')
+})
