dfedbdd213265fdc9ff399f7ada3bf4384c155cf
jin-co <lilijk@hanmail.net>
2022-05-11 13:20:22 -0400

    Completed preview

--- nodeJS/with_angular/post_back_test/src/app/components/posts/post-create/post-create.component.ts
+++ nodeJS/with_angular/post_back_test/src/app/components/posts/post-create/post-create.component.ts
@@ -92,6 +92,14 @@ export class PostCreateComponent implements OnInit {
 
   imgPreview!:string
   onImageAdd(e:Event) {
+    const file = (e.target as HTMLInputElement).files?.[0]
+    this.form.patchValue({image: file})
+    this.form.get('image')?.updateValueAndValidity()
+    const reader = new FileReader()
+    reader.onload = () => {
+      this.imgPreview = reader.result as string
+    }
 
+    reader.readAsDataURL(file as Blob)
   }
 }
