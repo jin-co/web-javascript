83b5df7865e809d11093e4ef77fb770589231228
jin-co <lilijk@hanmail.net>
2022-05-12 10:13:09 -0400

    Update

--- p/index.js
+++ p/index.js
@@ -1,10 +1,20 @@
-const btnUp = document.querySelector('.cursor-up')
-const btnDown = document.querySelector('.cursor-down')
+const btnUp = document.querySelector(".cursor-up");
+const btnDown = document.querySelector(".cursor-down");
+const leftPanelEls = document.querySelectorAll(".box-left div");
+const rightPanelEls = document.querySelectorAll(".box-right div");
+btnUp.addEventListener("click", () => {
+  console.log("up");
+});
 
-btnUp.addEventListener('click', () => {
-    console.log('up')
-})
+btnDown.addEventListener("click", () => {
+  console.log("down");
+  moveDown();
+});
 
-btnDown.addEventListener('click', () => {
-    console.log('down')
-})
+function moveDown() {
+  console.log(leftPanelEls);
+  for (let i = 0; i < leftPanelEls.length; i++) {    
+    leftPanelEls[i].style.transform = `translateY(-${100}%)`;
+    rightPanelEls[i].style.transform = "translateY(100%)";
+  }
+}
