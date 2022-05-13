cdc47247a8301be821b6aaf3be20cdd4ef8e1298
jin-co <lilijk@hanmail.net>
2022-05-13 12:34:53 -0400

    init image upload

--- nodeJS/with_angular/post_back_test/backend/routes/post.js
+++ nodeJS/with_angular/post_back_test/backend/routes/post.js
@@ -7,27 +7,7 @@ const jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //**file upload
-const multer = require('multer')
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-}
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype]
-    let error = new Error("invalid")
-    if(isValid) {
-      error = null
-    }
-    cb(error, "backend/images")
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-')
-    const ext = MIME_TYPE_MAP[file.mimetype]
-    cb(null, name + "-" + Date.now() + "." + ext)
-  }
-})
+
 //**/file upload
 
 router.get("", (req, res, next) => {
@@ -52,12 +32,10 @@ router.get("/:id", (req, res, next) => {
 //   });
 // });
 
-router.post("", jsonParser, multer({storage: storage}).single("image"), (req, res, next) => {  
-  const url = req.protocol + "://" + req.get("host")
+router.post("", jsonParser, (req, res, next) => {  
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename
   });
   post.save().then((data) => {
     res.status(201).json(data);
