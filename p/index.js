6ade690b43c857e23554c2b8d19be1bd6e85fb4f
jin-co <lilijk@hanmail.net>
2022-05-13 10:19:17 -0400

    Update

--- p/index.js
+++ p/index.js
@@ -2,10 +2,11 @@ const btnUp = document.querySelector(".cursor-up");
 const btnDown = document.querySelector(".cursor-down");
 const leftPanelEls = document.querySelectorAll(".box-left div");
 const rightPanelEls = document.querySelectorAll(".box-right div");
-btnUp.addEventListener("click", () => {
-  console.log("up");
-});
 let idx = 100
+
+btnUp.addEventListener("click", () => {
+  moveUp();
+});
 
 btnDown.addEventListener("click", () => {
   console.log("down");
@@ -16,7 +17,22 @@ function moveDown() {
   console.log(leftPanelEls);
   for (let i = 0; i < leftPanelEls.length; i++) {    
     leftPanelEls[i].style.transform = `translateY(-${idx}%)`;
-    rightPanelEls[i].style.transform = "translateY(100%)";
+    rightPanelEls[i].style.transform = `translateY(${-(300 - idx)}%)`;
   }
   idx += 100
+  if(idx >= 400) {
+    idx = 0
+  }
+}
+
+function moveUp() {
+  console.log(leftPanelEls);
+  for (let i = 0; i < leftPanelEls.length; i++) {    
+    leftPanelEls[i].style.transform = `translateY(-${idx}%)`;
+    rightPanelEls[i].style.transform = `translateY(${-(300 - idx)}%)`;
+  }
+  idx -= 100
+  if(idx < 0) {
+    idx = 400
+  }
 }
