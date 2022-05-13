b4df0562d2e2dd95c62df746355d2ea4a35a05cf
jin-co <lilijk@hanmail.net>
2022-05-13 11:14:09 -0400

    Completed multer test

--- nodeJS/with_angular/post_back_test/backend/routes/post.js
+++ nodeJS/with_angular/post_back_test/backend/routes/post.js
@@ -7,7 +7,27 @@ const jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //**file upload
-
+const multer = require('multer')
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg"
+}
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype]
+    const error = new Error("invalid")
+    if(isValid) {
+      error = null
+    }
+    cb(error, "backend/images")  
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-')
+    ext = MIME_TYPE_MAP[file.mimetype]
+    cb(null, name + '-' + Date.now() + '.' + ext)
+  }
+})
 //**/file upload
 
 router.get("", (req, res, next) => {
