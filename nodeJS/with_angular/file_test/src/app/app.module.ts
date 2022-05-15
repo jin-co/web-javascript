c5671cd775115bf6dedfc9dd133f6cc28b3457f0
jin-co <lilijk@hanmail.net>
2022-05-15 12:48:51 -0400

    Added components

--- nodeJS/with_angular/file_test/src/app/app.module.ts
+++ nodeJS/with_angular/file_test/src/app/app.module.ts
@@ -14,9 +14,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostCreateComponent, PostListComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
