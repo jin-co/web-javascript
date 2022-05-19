a10c980235832ddd9e1e09d1b8af8761f8ecfa20
jin-co <lilijk@hanmail.net>
2022-05-19 19:43:40 -0400

    Update

--- nodeJS/with_angular/p1_postApp/backend/middleware/check-auth.js
+++ nodeJS/with_angular/p1_postApp/backend/middleware/check-auth.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  console.log(req.body
-    .authorization)
+  // console.log('check auth header: ', req.headers)
+  // console.log('check auth header auth: ', req.headers.authorization)
+  // console.log('check auth body: ', req.body)
   try {
-    const token = req.header.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
     console.log('check auth backend:', token)
     jwt.verify(token, "secret_code");
     next()
@@ -12,4 +13,10 @@ module.exports = (req, res, next) => {
     console.log('try failed: ', error)
     res.status(400).json("middle auth failed");
   }
+
+  // jwt.verify(token, "secret_code", (err, user) => {
+  //   if(err) return res.sendStatus(403)
+  //   req.user = user
+  //   next()
+  // })
 };
--- nodeJS/with_angular/p1_postApp/src/app/services/user.service.ts
+++ nodeJS/with_angular/p1_postApp/src/app/services/user.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   baseURL: string = 'http://localhost:3000/user/';
   token!: string;
+  authStatusListener = new Subject<boolean>()
 
   constructor(private http: HttpClient) {}
 
