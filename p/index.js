229d99881482eaf494cd4577cdbad732e6f0997c
jin-co <lilijk@hanmail.net>
2022-05-20 10:20:35 -0400

    Completed

--- p/index.js
+++ p/index.js
@@ -1,60 +1,15 @@
-const card = document.querySelector(".card");
-const search = document.querySelector(".search");
-const main = document.querySelector("main");
-const URL = "https://api.github.com/users/";
+const img = document.querySelector('.img')
+const count = document.querySelector('.count')
 
-search.addEventListener("keydown", (e) => {
-  if (main.childNodes.length > 5) {
-    main.removeChild(main.lastChild);
-  }
-
-  if (e.key === "Enter") {
-    getData(e.target.value);
-  }
-});
-
-async function getData(name) {
-  const res = await fetch(URL + name);
-  const data = await res.json();
-  console.log(data.avatar_url);
-  createCard(data);
-}
-
-async function createCard(data) {
-  const card = document.createElement("div");
-  card.className = "card";
-  card.innerHTML = `    
-    <div class="img-box">
-      <img src="${data.avatar_url}" alt="${data.login} avatar" />
-    </div>
-
-    <div class="text-box">
-      <h2 class="name">${data.name}</h2>
-      <p class="text">
-        ${data.bio}
-      </p>
-      <div class="following-box">
-        <p>
-          <span class="count count-follower">${data.followers}</span>
-          a
-        </p>
-        <p>
-          <span class="count count-following">${data.following}</span>
-          a
-        </p>
-        <p>
-          <span class="count count-repos">${data.public_repos}</span>
-          a
-        </p>
-      </div>
-      <div class="extra-box">
-        <small class="tag">ah</small>
-        <small class="tag">ah</small>
-        <small class="tag">ah</small>
-        <small class="tag">ah</small>
-        <small class="tag">ah</small>
-      </div>
-    </div>  
-    `;
-  main.appendChild(card);
-}
+let idx = 0
+img.addEventListener('dblclick', (e) => {
+  console.log(e.offsetX, e.offsetY)
+  idx++
+  count.textContent = idx
+  const heartEl = document.createElement('i')  
+  heartEl.className = 'heart-effect'
+  heartEl.textContent = '❤️'
+  heartEl.style.top = `${e.offsetY}px`
+  heartEl.style.left = `${e.offsetX}px`
+  img.appendChild(heartEl)
+})
