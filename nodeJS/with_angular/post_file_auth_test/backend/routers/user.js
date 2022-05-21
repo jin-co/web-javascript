6ec43b1c04e383d3b1c0b1b41c328d1659c0dcf2
jin-co <lilijk@hanmail.net>
2022-05-21 10:30:35 -0400

    init bcrypt test

--- nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
+++ nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
@@ -2,26 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+
 
 router.post("", (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      console.log("hash: ", hash);
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user.save().then((result) => {
-        res.status(200).json(result);
-      }).catch(err => {
-          res.status(500).json(err)
-      })
-    })
-    .catch((err) => {        
-      res.status(500).json(err);
-    });
+
 });
 
 module.exports = router;
