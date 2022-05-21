7027f9a248d5d4f01253b1853b663d7331408078
jin-co <lilijk@hanmail.net>
2022-05-21 10:29:42 -0400

    Update

--- nodeJS/with_angular/post_file_auth_test/backend/models/user.js
+++ nodeJS/with_angular/post_file_auth_test/backend/models/user.js
@@ -3,3 +3,5 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+module.exports = mongoose.model("User", userSchema)
--- nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
+++ nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
@@ -14,8 +14,10 @@ router.post("", (req, res, next) => {
         password: hash,
       });
       user.save().then((result) => {
-        res.status(200).json();
-      });
+        res.status(200).json(result);
+      }).catch(err => {
+          res.status(500).json(err)
+      })
     })
     .catch((err) => {        
       res.status(500).json(err);
