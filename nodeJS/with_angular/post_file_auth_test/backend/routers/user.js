335e5c826c37d0bc53df91d0fb03fd410af332ae
jin-co <lilijk@hanmail.net>
2022-05-21 10:35:01 -0400

    Completed test

--- nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
+++ nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
@@ -2,10 +2,24 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-
+const bcrypt = require("bcrypt");
 
 router.post("", (req, res, next) => {
-
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => res.status(500).json(err));
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
