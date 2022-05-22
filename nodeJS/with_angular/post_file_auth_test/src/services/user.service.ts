c39c45d336025bb14a42fce200a52205d4458b94
jin-co <lilijk@hanmail.net>
2022-05-21 21:50:53 -0400

    Update

--- nodeJS/with_angular/post_file_auth_test/src/services/user.service.ts
+++ nodeJS/with_angular/post_file_auth_test/src/services/user.service.ts
@@ -6,6 +6,11 @@ import { User } from 'src/models/user';
 @Injectable({ providedIn: 'root' })
 export class UserService {
   private baseURL = 'http://localhost:3000/user/';
+
+  //
+  token!:string
+  //
+
   constructor(private http: HttpClient, private router: Router) {}
 
   addUser(email: string, password: string) {
@@ -14,8 +19,19 @@ export class UserService {
       password: password,
     };
     this.http.post(`${this.baseURL}signup`, user).subscribe((result) => {
-        console.log(result)
-        this.router.navigate(['/'])
+      console.log(result);
+      this.router.navigate(['/']);
+    });
+  }
+
+  signup(email: string, password: string) {
+    const user: User = {
+      email: email,
+      password: password,
+    };
+    this.http.post<{token:string, exp:number}>(`${this.baseURL}login`, user).subscribe((result) => {
+        this.token = result.token
+        
     });
   }
 }
