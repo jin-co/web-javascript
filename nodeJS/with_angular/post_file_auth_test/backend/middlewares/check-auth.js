014744294247d9297438a52f6067213074fd3888
jin-co <lilijk@hanmail.net>
2022-05-22 11:37:21 -0400

    init middleware test

--- nodeJS/with_angular/post_file_auth_test/backend/middlewares/check-auth.js
+++ nodeJS/with_angular/post_file_auth_test/backend/middlewares/check-auth.js
@@ -2,9 +2,6 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
 
-    const token = req.headers.authorization.split(' ')[1]
-    jwt.verify(token, "secret")
-    next()
 
     // const header = req.headers['authorization']
     // const token = authorization && authorization.split(' ')[1]
