fab53f1403aab4fa226056334e53994950078926
jin-co <lilijk@hanmail.net>
2022-05-22 11:40:36 -0400

    Completed

--- nodeJS/with_angular/post_file_auth_test/backend/middlewares/check-auth.js
+++ nodeJS/with_angular/post_file_auth_test/backend/middlewares/check-auth.js
@@ -1,14 +1,19 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-
-
-    // const header = req.headers['authorization']
-    // const token = authorization && authorization.split(' ')[1]
-    // if(!token) return res.sendStatus(401)
-    // jwt.verify(token, "secret", (err, user) => {
-    //     if(err) return res.sendStatus(403)
-    //     req.user = user
-    //     next()
-    // })
-}
+  try {
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, "secret")
+      next()
+  } catch {
+    res.status(403).json("middleware error")
+  }
+  // const header = req.headers['authorization']
+  // const token = authorization && authorization.split(' ')[1]
+  // if(!token) return res.sendStatus(401)
+  // jwt.verify(token, "secret", (err, user) => {
+  //     if(err) return res.sendStatus(403)
+  //     req.user = user
+  //     next()
+  // })
+};
