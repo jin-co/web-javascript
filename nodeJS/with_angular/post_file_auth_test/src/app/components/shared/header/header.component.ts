c42d42d259a1b7be5d7ee819c37ac074937bc241
jin-co <lilijk@hanmail.net>
2022-05-22 14:08:54 -0400

    Update

--- nodeJS/with_angular/post_file_auth_test/src/app/components/shared/header/header.component.ts
+++ nodeJS/with_angular/post_file_auth_test/src/app/components/shared/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean = false;
+  private authListenerSubs!: Subscription
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.authListenerSubs = 
+    this.userService.authUpdatedListener().subscribe(result => {
+      console.log(result)
+      this.isAuthenticated = result
+    })
   }
 
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe()
+  }
 }
--- nodeJS/with_angular/post_file_auth_test/src/services/user.service.ts
+++ nodeJS/with_angular/post_file_auth_test/src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 import { User } from 'src/models/user';
 
 @Injectable({ providedIn: 'root' })
@@ -9,6 +10,7 @@ export class UserService {
 
   //
   private token!:string
+  private authUpdated = new Subject<boolean>()
   //
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -31,11 +33,16 @@ export class UserService {
     }
     this.http.post<{token:string, exp:number}>(`${this.baseURL}login`, user).subscribe(data => {
       this.token = data.token
+      this.authUpdated.next(true)
     })
   }
   
   getToken() {
     return this.token
+  }
+
+  authUpdatedListener() {
+    return this.authUpdated.asObservable()
   }
 
 }
