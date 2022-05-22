4e40470000c8152e434e7aa0511514c6ad7854f6
jin-co <lilijk@hanmail.net>
2022-05-21 21:45:40 -0400

    Update

--- nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
+++ nodeJS/with_angular/post_file_auth_test/backend/routers/user.js
@@ -5,11 +5,47 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 router.post("/signup", (req, res, next) => {
-
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    user
+      .save()
+      .then((result) => [res.status(201).json(result)])
+      .catch((err) => {
+        res.status(401).json(err);
+      });
+  });
 });
 
 router.post("/login", (req, res, next) => {
-
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json("no user found");
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        res.status(401).json("wrong password");
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret",
+        { expiresIn: "1h" }
+      );
+      res.status.json({
+        token: token,
+        expiresIn: 3600,
+      });
+    })
+    .catch((err) => {
+      res.status(401).json(err);
+    });
 });
 
 module.exports = router;
