6d56578b8b68bb056ba4783b4411dde73abf25e3
jin-co <lilijk@hanmail.net>
2022-05-22 15:06:08 -0400

    Update

--- nodeJS/with_angular/post_file_auth_test/src/app/app-routing.module.ts
+++ nodeJS/with_angular/post_file_auth_test/src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './components/auth/login/login.component';
 import { SigninComponent } from './components/auth/signin/signin.component';
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { PostListComponent } from './components/posts/post-list/post-list.component';
 
 const routes: Routes = [
-  {path: '', component:PostListComponent},
-  {path: 'create', component:PostCreateComponent},
-  {path: 'edit/:id', component:PostCreateComponent},
-  {path: 'login', component:LoginComponent},
-  {path: 'signup', component:SigninComponent},
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent, },
+  {
+    path: 'edit/:id',
+    component: PostCreateComponent,
+    
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SigninComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],  
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
