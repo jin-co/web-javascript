e805d8cda8ffa860b84e14995af790208eb5b8c6
jin-co <lilijk@hanmail.net>
2022-05-24 11:09:51 -0400

    Update

--- nodeJS/with_angular/post_file_auth_test/backend/middleware/auth-check.js
+++ nodeJS/with_angular/post_file_auth_test/backend/middleware/auth-check.js
@@ -2,9 +2,16 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {    
   try {
+
+    console.log('auth check: ')    
+    console.log(req.headers)    
     const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, "secret")
-    req.next();
+    console.log('auth check token: ')    
+    console.log(token)   
+    console.log('auth check verify: ')    
+    console.log(jwt.verify(token, "secret"))   
+    next();
   } catch (error) {
     res.status(401).json(error);
   }
--- nodeJS/with_angular/post_file_auth_test/backend/routers/post.js
+++ nodeJS/with_angular/post_file_auth_test/backend/routers/post.js
@@ -35,9 +35,10 @@ router.get("", (req, res, next) => {
 
 router.post(
   "",
+  checkAuth,
   multer({ storage: storage }).single("image"), 
-  (req, res, next) => {
-    console.log(req.headers.authorization)    
+  (req, res, next) => {    
+    console.log('backend: ', req.headers)
     const url = req.protocol + "://" + req.get("host");
     const post = new Post({
       title: req.body.title,
@@ -50,7 +51,7 @@ router.post(
   }
 );
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     res.status(200).json("deleted");
   });
@@ -66,6 +67,7 @@ router.get("/:id", (req, res, next) => {
 
 router.put(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
