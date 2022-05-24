3cbb94648fc5e22842b5b7c07335cb35f430d9c7
jin-co <lilijk@hanmail.net>
2022-05-23 22:29:23 -0400

    Update

--- nodeJS/with_angular/post_file_auth_test/src/app/components/auth/auth.guard.ts
+++ nodeJS/with_angular/post_file_auth_test/src/app/components/auth/auth.guard.ts
@@ -1,9 +1,15 @@
+import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
+import { UserService } from "src/services/user.service";
 
+@Injectable()
 export class AuthGuard implements CanActivate {
+    constructor(private userService:UserService) {
+
+    }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        throw new Error("Method not implemented.");
+        
     }
 
 }
