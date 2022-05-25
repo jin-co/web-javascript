31da327b4d222834c4c031728811010c113ed6e9
jin-co <lilijk@hanmail.net>
2022-05-24 21:15:48 -0400

    Update

--- nodeJS/with_angular/post_back_test - auth/backend/models/user.js
+++ nodeJS/with_angular/post_back_test - auth/backend/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
-  email: { type: string, require: true, unique: true },
-  password: { type: string, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
 });
 module.exports = mongoose.model("User", userSchema);
--- nodeJS/with_angular/post_back_test - auth/backend/routes/user.js
+++ nodeJS/with_angular/post_back_test - auth/backend/routes/user.js
@@ -2,9 +2,20 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 router.post((req, res, next) => {
-        
+    bcrypt.hash(req.body.password, 10).then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        })
+        user.save().then(result => {
+
+        }).catch(err => {
+            res.status(401).json(err)
+        })
+    })
 })
 
 module.exports = router
