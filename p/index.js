0e0a8a3c26822e609b969edaae6da127e6494229
jin-co <lilijk@hanmail.net>
2022-05-26 10:21:15 -0400

    Update

--- p/index.js
+++ p/index.js
@@ -1,7 +1,9 @@
-const chkUpper = document.querySelector(".chk-upper");
-const chkLower = document.querySelector(".chk-lower");
-const chkNumber = document.querySelector(".chk-number");
-const chkSymbol = document.querySelector(".chk-symbol");
+// const chkUpper = document.querySelector(".chk-upper");
+// const chkLower = document.querySelector(".chk-lower");
+// const chkNumber = document.querySelector(".chk-number");
+// const chkSymbol = document.querySelector(".chk-symbol");
+const checkBoxesEls = document.querySelectorAll(".field input[type='checkbox']")
+
 const lengthEl = document.querySelector("#length");
 const form = document.querySelector(".form");
 const display = document.querySelector(".display");
@@ -14,25 +16,33 @@ let length = lengthEl.value;
 
 let generatedPassword = "";
 const upper = ["Z", "X", "C", "B", "N", "M", "H", "J", "K", "L", "U"];
-const lower = ["q", "w", "e", "r", "t", "a", "s", "d", "f", "g"];
+const lower = ["q", "w", "e", "r", "t", "a", "s", "d", "f", "g", "i"];
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const symbol = ["@", "#", "$", "%", "^", "&", "*", "(", "_"];
+const symbol = ["@", "#", "$", "%", "^", "&", "*", "(", "_", "+"];
 
-chkUpper.addEventListener("change", (e) => {
-  addUpper = e.target.checked;
+checks = [false, false, false, false]
+checkBoxesEls.forEach((chk, idx) => {
+    chk.addEventListener('change', (e) => {
+        console.log(idx)
+        checks[idx] = e.target.checked
+        console.log(checks)
+    })
 });
+// chkUpper.addEventListener("change", (e) => {
+//   addUpper = e.target.checked;
+// });
 
-chkLower.addEventListener("change", (e) => {
-  addLower = e.target.checked;
-});
+// chkLower.addEventListener("change", (e) => {
+//   addLower = e.target.checked;
+// });
 
-chkNumber.addEventListener("change", (e) => {
-  addNumber = e.target.checked;
-});
+// chkNumber.addEventListener("change", (e) => {
+//   addNumber = e.target.checked;
+// });
 
-chkSymbol.addEventListener("change", (e) => {
-  addSymbol = e.target.checked;
-});
+// chkSymbol.addEventListener("change", (e) => {
+//   addSymbol = e.target.checked;
+// });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -44,6 +54,7 @@ function createPassword() {
   generatedPassword = "";
   for (let i = 0; i < length; i++) {
     generatedPassword += Math.floor(Math.random() * length);
+    // generatedPassword += upper[Math.floor(Math.random() * upper.length)];
     console.log(generatedPassword);
   }
   display.value = generatedPassword;
