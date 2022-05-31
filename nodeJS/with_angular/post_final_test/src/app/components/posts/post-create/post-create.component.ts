2605aa61237815e6f5ccde861caa5fabf3468f7f
jin-co <lilijk@hanmail.net>
2022-05-31 12:33:27 -0400

    Update

--- nodeJS/with_angular/post_final_test/src/app/components/posts/post-create/post-create.component.ts
+++ nodeJS/with_angular/post_final_test/src/app/components/posts/post-create/post-create.component.ts
@@ -1,25 +1,31 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from 'src/app/models/post';
+import { PostService } from 'src/app/service/post.service';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
-  inputTitle:string = ''
-  inputContent:string = ''
-  outputTitle:string = ''
-  outputContent:string = ''
-  
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, { validators: [Validators.required] }),
+      content: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(1)],
+      }),
+    });
   }
 
   onClick() {
-    const post = {
-      title: this.inputTitle, 
-      content: this.inputContent 
-    }    
+    if (this.form.valid) {
+      this.postService.setPost(this.form.value.title, this.form.value.content);
+    }
+    this.form.reset();
   }
 }
