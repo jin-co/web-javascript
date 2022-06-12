b7e1cefb49851cf76dee49897899567cdf41b005
jin-co <lilijk@hanmail.net>
2022-06-12 12:49:07 +0900

    Added front folder

--- nodeJS/with_angular/post_test_final2/src/app/app.module.ts
+++ nodeJS/with_angular/post_test_final2/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { ErrorComponent } from './components/shared/error/error.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent,
+    ErrorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
