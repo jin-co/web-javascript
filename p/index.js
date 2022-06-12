874b6c65c8983256fc311eba22e5264f4c8c2a28
jin-co <lilijk@hanmail.net>
2022-06-13 08:31:09 +0900

    Completed drawing

---
+++ p/index.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext("2d")
+
+let x
+let y
+let touched = false
+
+canvas.addEventListener('mousedown', (e) => {
+    touched = true
+    x = e.offsetX
+    y = e.offsetY
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    touched = false
+})
+
+canvas.addEventListener('mousemove', (e) => {        
+    if(touched) {        
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+        ctx.lineTo(x2, y2)        
+        ctx.stroke()
+
+        x = x2
+        y = y2
+    }
+})
