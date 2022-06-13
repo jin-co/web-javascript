1caa535df84b1c5d3bc5406ab7abfb187aecea92
jin-co <lilijk@hanmail.net>
2022-06-14 07:20:01 +0900

    Added login form

--- nodeJS/with_angular/post_test_final2/src/app/app-routing.module.ts
+++ nodeJS/with_angular/post_test_final2/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { PostListComponent } from './components/posts/post-list/post-list.component';
 
@@ -7,6 +9,8 @@ const routes: Routes = [
   {path:'', component:PostListComponent},
   {path:'create', component:PostCreateComponent},
   {path:'edit/:id', component:PostCreateComponent},  
+  {path:'login', component:LoginComponent},  
+  {path:'signup', component:SignupComponent},  
 ];
 
 @NgModule({
--- nodeJS/with_angular/post_test_final2/src/app/components/auth/login/login.component.ts
+++ nodeJS/with_angular/post_test_final2/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  onSubmit(form:NgForm) {
+    if(form.valid) {
 
+    }    
+    form.resetForm()
+  }
 }
