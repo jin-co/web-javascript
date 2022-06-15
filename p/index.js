6be80392dd46647fe136d2fa4f3cd6154a58e73c
jin-co <lilijk@hanmail.net>
2022-06-15 09:59:31 +0900

    Update

--- p/index.js
+++ p/index.js
@@ -1,5 +1,7 @@
 const btnAddNote = document.querySelector(".btn-add-note");
 const body = document.querySelector("body");
+const notes = [];
+let btnsDelete;
 
 btnAddNote.addEventListener("click", () => {
   console.log("clicked");
@@ -16,6 +18,22 @@ function addNote() {
     </div>
     <textarea class="paper"></textarea>
   `;
+  notes.push(newNote);
   body.appendChild(newNote);
-  
+  btnsDelete = document.querySelectorAll(".delete");
+  btnsDelete.forEach((e, idx) => {
+    e.addEventListener("click", e.parentElement.parentElement.remove());
+  });
+}
+
+function deleteNote(idx) {
+  const notesEl = document.querySelectorAll(".note");
+  console.log(notesEl);
+  notesEl[idx].remove();
+  // console.log(idx)
+  // console.log(notes)
+  // delete notes[idx]
+  // notes.splice(idx, 1)
+  // console.log(notes)
+  // btnsDelete[idx].remove()
 }
