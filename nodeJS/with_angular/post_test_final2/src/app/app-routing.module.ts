1dbaf2b10686c6afa3c4ad3ea3617f024d33c8e3
jin-co <lilijk@hanmail.net>
2022-06-17 09:58:07 +0900

    Added auth guard

--- nodeJS/with_angular/post_test_final2/src/app/app-routing.module.ts
+++ nodeJS/with_angular/post_test_final2/src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/services/auth.guard';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { PostListComponent } from './components/posts/post-list/post-list.component';
 
 const routes: Routes = [
-  {path:'', component:PostListComponent},
-  {path:'create', component:PostCreateComponent},
-  {path:'edit/:id', component:PostCreateComponent},  
-  {path:'login', component:LoginComponent},  
-  {path:'signup', component:SignupComponent},  
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  {
+    path: 'edit/:id',
+    component: PostCreateComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ nodeJS/with_angular/post_test_final2/src/services/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { catchError, Observable } from 'rxjs';
+import { UserService } from './user';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+      if(!this.userService.getLogged()) {
+
+        this.router.navigate(['/login'])
+      }
+      return this.userService.getLogged()
+  }
+}
