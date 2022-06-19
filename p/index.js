b87046a0920d8918ed844f49f34073b222e76cac
jin-co <lilijk@hanmail.net>
2022-06-20 08:43:41 +0900

    Completed note app

--- p/index.js
+++ p/index.js
@@ -2,24 +2,54 @@ const btnAdd = document.querySelector('.add')
 
 btnAdd.addEventListener('click', () => addNote())
 
-function addNote() {
+function addNote(text = '') {
   const newNote = document.createElement('div')
   newNote.className = 'note'
   newNote.innerHTML = `
     <div class="note">
     <div class="tools">
-      <i class="btn fas fa-edit"></i>
-      <i class="btn fas fa-trash-alt"></i>
+      <i class="btn edit fas fa-edit"></i>
+      <i class="btn delete fas fa-trash-alt"></i>
     </div>
-    <textarea></textarea>
-    <div class="main hide"></div>
+    
+    <textarea class="${text ? "": "hide"}"></textarea>
+    <div class="main ${text ? "hide": ""}"></div>
   `
   document.body.appendChild(newNote)
+
+  const btnDelete = newNote.querySelector('.delete')
+  const btnEdit = newNote.querySelector('.edit')
+  const main = newNote.querySelector('.main')
+  const textarea = newNote.querySelector('textarea')
+
+  btnEdit.addEventListener('click', () => {
+    main.classList.toggle('hide')
+    textarea.classList.toggle('hide')
+  })
+
+  btnDelete.addEventListener('click', () => {
+    newNote.remove()
+    UpdateLS()
+  })
+
+  textarea.addEventListener('input', (e) => {
+    main.textContent = e.target.value
+    UpdateLS()
+  })
 }
 
+let noteList = []
 const notes = JSON.parse(localStorage.getItem('notes'))
 if(notes) {
   notes.forEach(note => {
     addNote(note)
   });
+}
+
+function UpdateLS() {
+  const createdNotes = document.querySelectorAll('.main')
+  createdNotes.forEach(note => {
+    noteList.push(note)
+  });
+  localStorage.setItem('notes', JSON.stringify(noteList))
 }
