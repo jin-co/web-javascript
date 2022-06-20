2269791d939c7a12584f31d907f05e17710319df
jin-co <lilijk@hanmail.net>
2022-06-20 10:18:40 +0900

    Update

--- nodeJS/with_angular/test-post-app/src/app/components/posts/post-create/post-create.component.ts
+++ nodeJS/with_angular/test-post-app/src/app/components/posts/post-create/post-create.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
+  inputTitle!: string;
+  inputText!: string;
+  outputTitle!: string;
+  outputText!: string;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick() {
+    this.outputTitle = this.inputTitle;
+    this.outputText = this.inputText;
   }
-
 }
