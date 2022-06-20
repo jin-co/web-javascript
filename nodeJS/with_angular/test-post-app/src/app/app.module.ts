62602bd2e3e66707b56f80cc25de2cde7ce1fc51
jin-co <lilijk@hanmail.net>
2022-06-20 09:47:41 +0900

    Added folders

--- nodeJS/with_angular/test-post-app/src/app/app.module.ts
+++ nodeJS/with_angular/test-post-app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,    
+    PostListComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
