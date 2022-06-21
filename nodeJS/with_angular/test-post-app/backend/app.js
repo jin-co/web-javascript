fdc54811833132d0a25b7de08371498ba48a5822
jin-co <lilijk@hanmail.net>
2022-06-21 10:26:09 +0900

    Connected to db and added get post

--- nodeJS/with_angular/test-post-app/backend/app.js
+++ nodeJS/with_angular/test-post-app/backend/app.js
@@ -2,25 +2,38 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Allow-Control-Access-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Allow-Control-Access-Headers",
-    "Origin, X-Requested-With, Content-Type ,Accept"
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
-    "Allow-Control-Access-Methods",
+    "Access-Control-Allow-Methods",
     "PUT, PATCH, DELETE, POST, GET, OPTIONS"
   );
 
   next();
 });
 
-app.use(express.json())
+app.use(express.json());
 
-const mongose = require('mongoose')
-
-app.get((req, res, next) => {
-  
-})
+const mongoose = require("mongoose");
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+});
+const Post = mongoose.model("Post", postSchema);
+const dbURL = "mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/post-app?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURL)
+  .then(() => console.log("connected"))
+  .catch(() => console.log("failed"));
+app.get("/posts", (req, res, next) => {
+  Post.find().then(posts => {
+    console.log('back get: ')
+    console.log(posts)
+    res.status(200).json(posts)
+  })
+});
 
 module.exports = app;
