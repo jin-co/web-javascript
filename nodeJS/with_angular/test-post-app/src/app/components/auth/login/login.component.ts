0d2efb51828ed04d2b4e04c3ca45171a30b4ad57
jin-co <lilijk@hanmail.net>
2022-06-22 10:46:43 +0900

    Added login form

--- nodeJS/with_angular/test-post-app/src/app/components/auth/login/login.component.ts
+++ nodeJS/with_angular/test-post-app/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/services/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form:NgForm) {
+    if(form.valid) {
+      console.log(form.value)
+    }
+    form.resetForm()
+  }
 }
--- nodeJS/with_angular/test-post-app/src/services/user.ts
+++ nodeJS/with_angular/test-post-app/src/services/user.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 
+@Injectable({providedIn: 'root'})
 export class UserService {
   private baseURL = 'http://localhost:3000/auth/'
 
