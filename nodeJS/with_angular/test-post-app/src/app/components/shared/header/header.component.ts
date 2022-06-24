2f9f7f8e4be02ee018027c2243b63ec43c5e5ac2
jin-co <lilijk@hanmail.net>
2022-06-24 09:35:28 +0900

    Updated nav to toggle menu when login

--- nodeJS/with_angular/test-post-app/src/app/components/shared/header/header.component.ts
+++ nodeJS/with_angular/test-post-app/src/app/components/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/services/user';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLogged:boolean = false
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.authUpdateListener().subscribe(result => {
+      this.isLogged = result
+    })
   }
 
 }
