92a8f34421be8a2ed489793c0917d2aabf8dab9d
jin-co <lilijk@hanmail.net>
2022-06-27 10:40:40 +0900

    Added error message

--- nodeJS/with_angular/test-post-app/src/app/app.module.ts
+++ nodeJS/with_angular/test-post-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 import { ErrorComponent } from './components/shared/error/error.component';
 import { AuthInterceptor } from './components/auth/auth.interceptor';
+import { ErrorInterceptor } from './error.interceptor';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,7 @@ import { AuthInterceptor } from './components/auth/auth.interceptor';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorComponent, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
