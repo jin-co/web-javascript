7a4a226a2ab3b2d2f208f5a42e48058bc9979d54
jin-co <lilijk@hanmail.net>
2022-06-28 10:01:14 +0900

    Clean

--- nodeJS/with_angular/post_back_test - auth/backend/models/user.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
-});
-module.exports = mongoose.model("User", userSchema);
