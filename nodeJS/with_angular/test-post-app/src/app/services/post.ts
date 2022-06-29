5e718e748230b58e685bf9765e12b8a107edae4b
jin-co <lilijk@hanmail.net>
2022-06-29 10:50:34 +0900

    Added service and model for the front size

---
+++ nodeJS/with_angular/test-post-app/src/app/services/post.ts
@@ -0,0 +1,8 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class PostService {
+  constructor(private http: HttpClient, private router: Router) {}
+}
