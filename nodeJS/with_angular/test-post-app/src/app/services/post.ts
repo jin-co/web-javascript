b4277622f51eb0db360ec8b3b9ea05db0a0977cf
jin-co <lilijk@hanmail.net>
2022-06-29 10:57:17 +0900

    Added login form

--- nodeJS/with_angular/test-post-app/src/app/services/post.ts
+++ nodeJS/with_angular/test-post-app/src/app/services/post.ts
@@ -1,8 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Post } from '../models/post';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
+  private baseURL = 'http://localhost:3000/posts/';
+  private postUpdate = new Subject<Post[]>();
+
   constructor(private http: HttpClient, private router: Router) {}
+
+  getPosts() {}
+
+  getPost(id: string) {}
+
+  addPost(title: string, content: string) {}
+
+  deletePost(id: string) {}
+
+  updatePost(id: string, title: string, content: string) {}
+
+  postUpdateListener() {
+    return this.postUpdate.asObservable()
+  }
 }
