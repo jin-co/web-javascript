06bfdbe801d88acf5bfa4211193e6ea571c3e1ba
jin-co <lilijk@hanmail.net>
2022-06-30 10:49:34 +0900

    cennected db

--- nodeJS/with_angular/test-post-app/backend/app.js
+++ nodeJS/with_angular/test-post-app/backend/app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  imagePath: { type: String },
+});
+const Post = mongoose.model("Post", postSchema);
+const dbURL =
+  "mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/app-post?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURL)
+  .then(() => console.log("connected"))
+  .catch(() => console.log("connection failed"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
