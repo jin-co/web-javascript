13a590510a74aff3787943ae250bff39e7f48cc5
jin-co <lilijk@hanmail.net>
2022-07-06 08:50:15 +0900

    Update

--- p/index.js
+++ p/index.js
@@ -28,3 +28,9 @@ canvas.addEventListener('mousemove', (e) => {
     y = y2
   }
 })
+
+
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const replay = document.querySelector('#replay')
