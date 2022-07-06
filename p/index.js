00e6b4ff6de276be2e18f7a7554d3af4e45e12e9
jin-co <lilijk@hanmail.net>
2022-07-07 08:46:15 +0900

    Update

--- p/index.js
+++ p/index.js
@@ -1,36 +1,37 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext("2d")
-
-let x
-let y
-let clicked = false
-
-canvas.addEventListener('mouseup', (e) => {
-  clicked = false
-})
-
-canvas.addEventListener('mousedown', (e) => {
-  clicked = true
-  x = e.offsetX
-  y = e.offsetY
-})
-
-canvas.addEventListener('mousemove', (e) => {
-  if(clicked) {
-    const x2 = e.offsetX
-    const y2 = e.offsetY
-    ctx.beginPath()
-    ctx.moveTo(x, y)
-    ctx.lineTo(x2, y2)
-    ctx.stroke()
-
-    x = x2
-    y = y2
-  }
-})
-
-
 const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
+
+runAnimation()
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    num.addEventListener('animationend', (e) => {
+      console.log(e)
+      if (e.animationName === 'goIn' ) {
+        num.classList.remove('in')
+        num.classList.add('out')
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in')
+      } else {
+        counter.classList.add('hide')
+        finalMessage.classList.add('show')
+      }
+    })
+  })
+}
+
+function resetDOM() {
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
+  nums.forEach((num) => {
+    num.classList.value = ''
+  })
+  nums[0].classList.add('in')
+}
+
+replay.addEventListener('click', () => {
+  resetDOM()
+  runAnimation()
+})
