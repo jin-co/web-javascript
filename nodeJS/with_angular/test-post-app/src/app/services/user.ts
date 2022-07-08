21f81f804316675b10f52875c797f2022bd98a69
jin-co <lilijk@hanmail.net>
2022-07-08 10:09:10 +0900

    Added auto auth

--- nodeJS/with_angular/test-post-app/src/app/services/user.ts
+++ nodeJS/with_angular/test-post-app/src/app/services/user.ts
@@ -10,6 +10,7 @@ export class UserService {
   private token!: string;
   private isLogged: boolean = false;
   private userUpdate = new Subject<boolean>();
+  private tokenTimer:any
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -36,16 +37,18 @@ export class UserService {
         this.token = data.token;
         this.userUpdate.next(true);
 
-        const now = new Date()
-        const expDate = new Date(now.getTime() + data.exp * 1000)
+        const now = new Date();
+        const expDate = new Date(now.getTime() + data.exp * 1000);
 
-        this.saveAuthData(data.token, expDate)
+        this.saveAuthData(data.token, expDate);
+
+        this.setAuthTimer(data.exp)
         this.router.navigate(['/']);
       });
   }
 
   logout() {
-    this.clearAuthData()
+    this.clearAuthData();
   }
 
   private saveAuthData(token: string, exp: Date) {
@@ -56,6 +59,39 @@ export class UserService {
   private clearAuthData() {
     localStorage.removeItem('token');
     localStorage.removeItem('exp');
+  }
+
+  autoAuth() {
+    const authInfo = this.getAuthData();
+    const now = new Date()
+    if(typeof(authInfo) !== 'undefined') {
+      const isInFuture = authInfo.exp.getTime() - now.getTime()
+      
+      if(isInFuture > 0) {
+        this.token = authInfo.token
+        this.isLogged = true
+        this.setAuthTimer(isInFuture / 1000)
+        this.userUpdate.next(true)
+      }
+    }
+  }
+
+  private setAuthTimer(duration:number) {
+    this.tokenTimer = setTimeout(() => {
+      this.logout()
+    }, duration * 1000);
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const exp = localStorage.getItem('exp');
+    if (!token || !exp) {
+      return;
+    }
+    return {
+      token: token,
+      exp: new Date(exp),
+    };
   }
 
   getToken() {
