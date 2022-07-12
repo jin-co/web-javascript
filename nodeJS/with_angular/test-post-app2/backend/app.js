3157749af061e3fa4b00b1d3ffb2923ea3457152
jin-co <lilijk@hanmail.net>
2022-07-12 09:43:40 +0900

    Added backend

---
+++ nodeJS/with_angular/test-post-app2/backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, DELETE, POST, PUT, PATCH, OPTIONS"
+  );
+
+  next();
+});
+
+module.exports = app;
