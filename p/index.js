6009cf40583d4e8f8c7905701254098569094d32
jin-co <lilijk@hanmail.net>
2022-07-18 09:26:37 +0900

    Update

--- p/index.js
+++ p/index.js
@@ -5,6 +5,11 @@ const imgs = document.querySelector('.img-box')
 
 let idx = 0
 
+let inter = setInterval(() => {
+  idx++
+  changeImage()
+}, 2000);
+
 function changeImage() {
   if (idx > imgEls.length - 1) {
     idx = 0
@@ -19,11 +24,18 @@ function changeImage() {
 prev.addEventListener('click', () => {
   idx--
   changeImage()
+  resetIn()
   console.log('prev', idx)  
 })
 
 next.addEventListener('click', () => {
   idx++
   changeImage()
+  resetIn()
   console.log('next', idx)  
 })
+
+function resetIn() {
+  clearInterval(inter)
+  inter = setInterval(changeImage, 2000)
+}
