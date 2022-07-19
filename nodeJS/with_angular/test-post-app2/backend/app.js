bc0c247388090fa75f603561eb09a1368ff9ee73
jin-co <lilijk@hanmail.net>
2022-07-19 10:27:19 +0900

    Updaet

--- nodeJS/with_angular/test-post-app2/backend/app.js
+++ nodeJS/with_angular/test-post-app2/backend/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+});
+const Post = mongoose.model("Post", postSchema)
+const DB_URL = 'mongodb+srv://1234:1234@cluster0.yz15b.mongodb.net/post-app?retryWrites=true&w=majority'
+mongoose.connect(DB_URL).then(() => console.log('connected')).catch(() => console.log('failed'))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,5 +22,13 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use(express.json());
+
+app.get("/posts", (req, res, next) => {
+  Post.find().then(result => {
+    res.status(200).json(result)
+  })
+})
 
 module.exports = app;
--- nodeJS/with_angular/test-post-app2/src/app/services/post.service.ts
+++ nodeJS/with_angular/test-post-app2/src/app/services/post.service.ts
@@ -1,6 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Post } from '../models/post.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
-  constructor() {}
+  private posts: Post[] = [];
+  private postUpdate = new Subject<Post[]>();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  getPosts() {}
+
+  getPost(id: string) {}
+
+  addPost(title: string, content: string) {}
+
+  deletePost(id: string) {}
+
+  updatePost(id: string, title: string, content: string) {}
 }
