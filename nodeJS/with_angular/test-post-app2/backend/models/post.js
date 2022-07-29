4c6927e76d37be653814ccefea6634b5fc44a56e
jin-co <lilijk@hanmail.net>
2022-07-29 10:14:24 +0900

    Clean

--- nodeJS/with_angular/test-post-app2/backend/models/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const postSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  auth: { type: mongoose.Schema.Types.ObjectId, required: true },
-  imagePath: { type: String },
-});
-
-module.exports = mongoose.model("Post", postSchema);
