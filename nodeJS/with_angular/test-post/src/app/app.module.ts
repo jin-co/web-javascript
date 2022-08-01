aba797e5a3c81920425b4881ff833fd3dcc59338
jin-co <lilijk@hanmail.net>
2022-08-01 14:54:24 +0900

    Added comps

--- nodeJS/with_angular/test-post/src/app/app.module.ts
+++ nodeJS/with_angular/test-post/src/app/app.module.ts
@@ -5,9 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material-module';
+import { PostCreateComponent } from './component/posts/post-create/post-create.component';
+import { PostListComponent } from './component/posts/post-list/post-list.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { ErrorComponent } from './component/shared/error/error.component';
+import { LoginComponent } from './component/auth/login/login.component';
+import { SignupComponent } from './component/auth/signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent,
+    ErrorComponent,
+    LoginComponent,
+    SignupComponent,    
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
