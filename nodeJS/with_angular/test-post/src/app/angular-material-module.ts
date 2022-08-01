bfae238fd2e90e9d648ed6d3df3459bd6085c903
jin-co <lilijk@hanmail.net>
2022-08-01 14:50:41 +0900

    Added angular material

---
+++ nodeJS/with_angular/test-post/src/app/angular-material-module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  exports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
+})
+export class AngularMaterialModule {}
